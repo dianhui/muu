1c6ddb2555796c010782b1ae42545f1dc55108d0
dianhui <dong.hui1985@gmail.com>
2014-04-04 10:24:29 +0800

    Add share drop down view in read page.

--- muuUIDemo/src/com/muu/ui/ReadPageActivity.java
+++ muuUIDemo/src/com/muu/ui/ReadPageActivity.java
@@ -5,12 +5,16 @@ import com.muu.widget.TouchImageView;
 import com.muu.widget.TouchImageView.OnGestureListener;
 
 import android.app.Activity;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+import android.widget.RelativeLayout.LayoutParams;
 
 public class ReadPageActivity extends Activity implements OnGestureListener {
 	private RelativeLayout mActionBarLayout = null;
@@ -19,19 +23,29 @@ public class ReadPageActivity extends Activity implements OnGestureListener {
 	private TouchImageView mContentImage = null;
 	private Boolean mTouchedMode = false;
 	private Boolean mRecomment = false;
+	private View mShareDropView = null;
+	private PopupWindow mSharePopup = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.read_page_layout);
 		
+		setupActionBar();
+		setupContentView();
+		setupBottomView();
 		
+	}
+
+	private void setupActionBar() {
 		mActionBarLayout = (RelativeLayout)this.findViewById(R.id.rl_action_bar);
-		mPageIdxLayout = (RelativeLayout)this.findViewById(R.id.rl_page_index);
-		mBottomLayout = (RelativeLayout)this.findViewById(R.id.rl_bottom_chapter);
-		
-		mContentImage = (TouchImageView)this.findViewById(R.id.imv_content);
-		mContentImage.setSwipObserver(this);
+		RelativeLayout backBtn = (RelativeLayout)this.findViewById(R.id.rl_back);
+		backBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ReadPageActivity.this.finish();
+			}
+		});
 		
 		final ImageButton recommentBtn = (ImageButton)this.findViewById(R.id.imv_btn_recomment);
 		recommentBtn.setOnClickListener(new OnClickListener() {
@@ -47,15 +61,38 @@ public class ReadPageActivity extends Activity implements OnGestureListener {
 			}
 		});
 		
-		RelativeLayout backBtn = (RelativeLayout)this.findViewById(R.id.rl_back);
-		backBtn.setOnClickListener(new OnClickListener() {
+		setupDropdownView();
+	}
+	
+	private void setupDropdownView() {
+		mShareDropView = LayoutInflater.from(this).inflate(
+				R.layout.share_drop_down_layout, null);
+		mSharePopup = new PopupWindow(mShareDropView,
+		        LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, true);
+		mSharePopup.setTouchable(true);
+		mSharePopup.setOutsideTouchable(true);
+		mSharePopup.setBackgroundDrawable(new ColorDrawable(0));
+		
+		final ImageButton shareBtn = (ImageButton)this.findViewById(R.id.imv_btn_share);
+		shareBtn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				ReadPageActivity.this.finish();
+				mSharePopup.showAsDropDown(shareBtn, -28, 24);
 			}
 		});
+		
 	}
 
+	private void setupContentView() {
+		mContentImage = (TouchImageView)this.findViewById(R.id.imv_content);
+		mContentImage.setSwipObserver(this);
+	}
+	
+	private void setupBottomView() {
+		mPageIdxLayout = (RelativeLayout)this.findViewById(R.id.rl_page_index);
+		mBottomLayout = (RelativeLayout)this.findViewById(R.id.rl_bottom_chapter);
+	}
+	
 	@Override
 	public void onSwipPrevious() {
 		Toast.makeText(getApplicationContext(), "onSwipPrevious",
