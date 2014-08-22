4f23b3fd6988c5b596f3f49943421670d8678100
dianhui <dong.hui1985@gmail.com>
2014-08-22 10:52:09 +0800

    Add activity dialog for share

---
+++ muuUIDemo/src/com/muu/ui/ShareActivityDialog.java
@@ -0,0 +1,16 @@
+package com.muu.ui;
+
+import com.muu.cartoon.test.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShareActivityDialog extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.share_layout);
+		
+	}
+
+}
